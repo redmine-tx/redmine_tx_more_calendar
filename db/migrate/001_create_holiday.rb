4ae3ff62b764523d1e6d86a04bdddab731f2fca6
KiHyun Kang <testors@gmail.com>
2026-01-29 17:10:05 +0900

    Update redmine_tx_more_calendar - 2026-01-29 17:10:05

--- db/migrate/001_create_holiday.rb
+++ db/migrate/001_create_holiday.rb
@@ -1,10 +1,15 @@
 class CreateHoliday < ActiveRecord::Migration[6.1]
-  def change
-    create_table :holidays do |t|
-      t.date :date
-      t.string :name, charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci'
-      t.string :description, charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci'
+  def up
+    unless table_exists?(:holidays)
+      create_table :holidays do |t|
+        t.date :date
+        t.string :name, charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci'
+        t.string :description, charset: 'utf8mb4', collation: 'utf8mb4_unicode_ci'
+      end
     end
   end
-end
 
+  def down
+    drop_table :holidays if table_exists?(:holidays)
+  end
+end
